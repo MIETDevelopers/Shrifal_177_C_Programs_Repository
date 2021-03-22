08a3251597dae8e67a3885bc8b60895a76bb170b
unknown <2020a1r177@mietjammu.in>
2021-03-22 13:34:25 +0530

    hello world file

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
+//Name:Shrifal raina
+//Date:22-3-2021
+//purpose: Hello world
+#include <stdio.h> //header file
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
-   return 0;
+   return 0;//return statement
 }
